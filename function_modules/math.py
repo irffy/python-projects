6ca97740e375457f9eacdd89da8fa89c34657b40
irffy <61529005+irffy@users.noreply.github.com>
2025-01-15 17:45:42 +0000

    Added functions and modules

---
+++ function_modules/math.py
@@ -0,0 +1,17 @@
+def sum(num1, num2):
+    return num1+num2
+
+def sub(num1, num2):
+    return num1-num2
+
+def mul(num1, num2):
+    return num1*num2
+
+def div(num1, num2):
+    return num1/num2
+
+# a=sum(10,2)
+# b=sub(10,2)
+# c=mul(10,2)
+# d=div(10,2)
+# # print(a,b,c,d)
---
+++ function_modules/math_mod.py
@@ -0,0 +1,7 @@
+# import math
+# a = math.sum(10,2)
+# print(a)
+
+import math as m
+a = m.sum(10,2)
+print(a)
