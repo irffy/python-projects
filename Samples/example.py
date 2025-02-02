26440076c282c87c2af5f12cada89c1e48126e23
Irfan Ahmed <61529005+irffy@users.noreply.github.com>
2025-02-02 16:37:35 +0000

    Added samples

--- Samples/example.py
+++ Samples/example.py
@@ -6,8 +6,23 @@
 # # add = lis[0]+lis[2]
 # # print(add)
 
-str = "Hello Irfan!"
-x= str.split(" ")
-print(x)
-y = str.count("l")
-print(y)
+# str = "Hello Irfan!"
+# x= str.split(" ")
+# print(x)
+# y = str.count("l")
+# print(y)
+# z= str.strip("!")
+# print(z)
+# text = "___Some spaces around___"
+# stripped_text = text.strip("_")
+# print("Stripped text:", stripped_text)
+
+import re
+text = "apple,banana,orange,grape"
+pattern = "apple"
+res = re.search(pattern,text)
+print(res)
+if res:
+    print("Pattern found")
+else:
+    print("String not found")    
