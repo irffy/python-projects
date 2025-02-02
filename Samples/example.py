9fc0d2d770c45c850540c437675b6c9d3ca9cf36
Irfan Ahmed <61529005+irffy@users.noreply.github.com>
2025-02-02 16:29:03 +0000

    Added samples

---
+++ Samples/example.py
@@ -0,0 +1,13 @@
+# lis = [1,2,3,4]
+# # print(lis)
+# # lis.append(5)
+# # lis.remove(2)
+# # print(lis)
+# # add = lis[0]+lis[2]
+# # print(add)
+
+str = "Hello Irfan!"
+x= str.split(" ")
+print(x)
+y = str.count("l")
+print(y)
